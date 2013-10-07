9ae7d8bd4960802c341f96ae12dcfd2d06dd34a7
Joseph Siddall <joseph@omlet.co.uk>
2013-10-07 09:19:37 +0100

    added a test class which tests ChannelDebug

---
+++ test/ChannelDebugTest.py
@@ -0,0 +1,30 @@
+from ChannelDebug import ChannelDebug
+from unittest import TestCase
+from model.Person import Person
+
+class ChannelDebugTest(TestCase):
+
+    def setUp(self):
+        self.d = ChannelDebug('http://192.168.2.17', '1025', 'hello/world')
+
+
+    def testLogStringDoesNotThrowException(self):
+        self.d.log("hello")
+
+    def testLogIntDoesNotThrowException(self):
+        self.d.log(44)
+
+    def testLogNullDoesNotThrowException(self):
+        self.d.log(None)
+
+    def testLogObjectDoesNotThrowException(self):
+        peterGriffin = Person('Peter Griffin', 45)
+        chrisGriffin = Person('Chris Griffin', 15, peterGriffin)
+        self.d.log(chrisGriffin)
+
+    def testLogRecursionDoesNotThrowException(self):
+        class Node(object): pass
+        n1, n2 = Node(), Node()
+        n1.neighbour, n2.neighbour = n2, n1
+        self.d.log(n1)
+
